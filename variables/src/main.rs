f28f83ac21a6a10a96b11281196f6d289d9406af
n18011 <n18011@std.it-college.ac.jp>
2019-12-14 17:21:28 +0900

    数値演算

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -22,8 +22,24 @@ fn main (){
     let guess = "42".parse().expect("Not a number!");    // 数字ではありません！
     println!("guess is {}", guess);
     */
+    /*
     let x = 2.0; // f64
 
     let y: f32 = 3.0; // f32
+    */
 
+    // 足し算
+    let sum = 5 + 10;
+
+    // 引き算
+    let difference = 95.5 - 4.3;
+
+    // 掛け算
+    let product = 4 * 30;
+
+    // 割り算
+    let quotient = 56.7 / 32.2;
+
+    // 余り
+    let remainder = 43 % 5;
 }
