5aa0a40ad47614169284e5163b65214292727f7c
n18011 <n18011@std.it-college.ac.jp>
2019-12-15 17:36:59 +0900

    add 配列型

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -56,6 +56,7 @@ fn main (){
     let heart_eyed_cat = '😻';
     */
 
+    /*
     let tup: (i32, f64, u8) = (500, 6.3, 1);
     // let (x, y, z) = (500, 6.3, 1);
     // println!("The value of tup.2 is: {}", tup.2);
@@ -65,5 +66,19 @@ fn main (){
     println!("The value of five_hundred is: {}", five_hundred);
     println!("The value of six_point_three is: {}", six_point_three);
     println!("The value of one is: {}", one);
+    */
 
+    let a = [1,2,3,4,5];
+    /*
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+                  "August", "September", "October", "November", "December"];
+                  */
+    /*
+    let first = a[0];
+    let second = a[1];
+    println!("first and second: {} and {}", first, second);
+    */
+    let index = 10;
+    let element = a[index];
+    println!("The value of element is: {}", element);
 }
