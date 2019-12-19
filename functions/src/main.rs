9d3f4fae26ae42e83b27958d95885d4dab37c328
n18011 <n18011@std.it-college.ac.jp>
2019-12-19 15:48:21 +0900

    fin 関数の動作法

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -11,12 +11,34 @@
     // another_function(5, 6);
     // let y = 6; // これは 文
     // let x = (let y = 6); // error:letは文なため値を返さない.よって変数に代入できない。
+    /*
     let x = 5;
     let y = {
         let x = 3;
         x + 1
     };
     println!("The value of y is {}", y);
+    */
+     /*
+    let x = five();
+    println!("The value of x is {}", x);
+    */
+    let x = plus_one(5);
+    println!("The value of x is {}", x);
+
+}
+
+/*
+fn plus_one(x:i32) ->i32 {
+    x + 1
+}
+*/
+fn plus_one(x:i32) ->i32 {
+    x + 1;
+}
+
+fn five() -> i32 {
+    5
 }
 
 /*
