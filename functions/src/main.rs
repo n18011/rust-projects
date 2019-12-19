8722e911e29834673ee8d9faf9c7bebd495c16c3
n18011 <n18011@std.it-college.ac.jp>
2019-12-19 15:09:32 +0900

    add 関数 and 関数の引数

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+ /*関数の動作法
+  */
+ fn main() {
+     /*
     println!("Hello, world!");
+
+    another_function();
+    */
+
+     //another_function(5);
+    another_function(5, 6);
+}
+
+/*
+fn another_function() {
+    println!("Another function.");  // 別の関数
+}
+*/
+/*
+fn another_function(x:i32) {
+    println!("the value of x is {}", x);
+}
+*/
+fn another_function(x: i32, y: i32) {
+    println!("the value of x is {}", x);
+    println!("the value of y is {}", y);
 }
