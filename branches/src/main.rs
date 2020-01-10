cb2254280a521dda411d532c8f21a267a7d2c589
n18011 <n18011@std.it-college.ac.jp>
2020-01-11 01:00:30 +0900

    else if

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -9,10 +9,27 @@ fn main() {
         println!("condition was false");      // 条件は偽でした
     }
     */
+    /*
     let number = 3;
 
     if number {
         println!("number was three");     // 数値は3です
+    }
+    */
+    let number = 6;
+
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
+    } else {
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
     }
 
 }
