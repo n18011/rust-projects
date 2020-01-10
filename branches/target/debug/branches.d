24db49ab1efaad9be915106bf254b651a6c1b55c
n18011 <n18011@std.it-college.ac.jp>
2020-01-11 00:14:47 +0900

    initial

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/n18011/rust-projects/branches/target/debug/branches: /home/n18011/rust-projects/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-9d06b63071382c11.d
@@ -0,0 +1,5 @@
+/home/n18011/rust-projects/branches/target/debug/deps/branches-9d06b63071382c11: src/main.rs
+
+/home/n18011/rust-projects/branches/target/debug/deps/branches-9d06b63071382c11.d: src/main.rs
+
+src/main.rs:
