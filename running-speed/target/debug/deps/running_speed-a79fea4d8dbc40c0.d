5ab43d853fbaf2eb3b0d37191bdba809044635d7
n18011 <n18011@std.it-college.ac.jp>
2020-01-24 23:11:02 +0900

    hello running speed

---
+++ running-speed/target/debug/deps/running_speed-a79fea4d8dbc40c0.d
@@ -0,0 +1,5 @@
+/home/n18011/rust-projects/running-speed/target/debug/deps/running_speed-a79fea4d8dbc40c0: src/main.rs
+
+/home/n18011/rust-projects/running-speed/target/debug/deps/running_speed-a79fea4d8dbc40c0.d: src/main.rs
+
+src/main.rs:
---
+++ running-speed/target/debug/running-speed.d
@@ -0,0 +1,1 @@
+/home/n18011/rust-projects/running-speed/target/debug/running-speed: /home/n18011/rust-projects/running-speed/src/main.rs
