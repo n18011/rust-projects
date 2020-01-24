0863217fc2e556a729b37c14ae5303fe84ec49bb
n18011 <n18011@std.it-college.ac.jp>
2020-01-24 23:14:40 +0900

    complete running speed

--- running-speed/src/main.rs
+++ running-speed/src/main.rs
@@ -1,3 +1,9 @@
+use std::time::{Instant};
 fn main() {
+    let start = Instant::now();
+    // ここから実行速度を確かめたい関数を実行する
     println!("Hello, world!");
+    // ここまでの間に
+    let end = start.elapsed();
+    println!("{}.{:08}sec", end.as_secs(), end.subsec_nanos());
 }
